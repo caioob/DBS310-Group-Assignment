76768e51180f2840b6484b02f5bd904940b2793e
Caio Leonardo Bueno Basaglia <clbueno-basaglia@myseneca.ca>
2019-02-15 11:01:05 -0500

    Task 3 completed

---
+++ A1_ID1#_ID2#.sql
@@ -0,0 +1,17 @@
+SELECT last_name,
+        salary,
+        job_id,
+        nvl(to_char(employees.manager_id), 'None') AS "Manager#",
+        department_name,
+        to_char(((salary + salary * nvl(commission_pct, 0) + 1000) * 12), '$99,999,000.00') AS "Annual Salary" 
+    FROM employees INNER JOIN departments 
+            ON departments.department_id = employees.department_id
+    WHERE (commission_pct IS NULL OR upper(department_name) LIKE 'SALES') AND salary + salary * nvl(commission_pct, 0) + 1000 > 15000
+    ORDER BY salary DESC;
+
+
+SELECT *
+    FROM employees;
+
+SELECT *
+    FROM departments;
